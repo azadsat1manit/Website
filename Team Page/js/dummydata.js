8e43282d51b15f7b263a74bb7b5aa943751f74f9
azadsat1manit <55313805+azadsat1manit@users.noreply.github.com>
2019-10-15 20:14:29 +0530

    Add Team Page

---
+++ Team Page/js/dummydata.js
@@ -0,0 +1,38 @@
+// grid dummy data
+var dummyData = {
+	"IV-OBDH":"<h1>OBDH</h1>",
+	
+	"IV-TTC":"<h1>TTC</h1><li class=\"product\"><div class=\"member member--picture\"></div></li>",
+
+	"IV-Payload":"<h1>Payload</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IVPayload_SparshSingh_Mech.jpg\" id=\"picmem\" title=\"member\"><p>Sparsh Singh<p><br>Mech.</div></li><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IVPayload_AnupamAgrawal_CSE.jpeg\" id=\"picmem\" title=\"member\"><p>Anupam Agrawal</p><br>CSE</div></li>",
+	
+	"IV-STM":"<h1>STM</h1><li class=\"product\"><div class=\"member member--picture\"></div></li>",
+	
+	"IV-EPS":"<h1>EPS</h1><li class=\"product\"><div class=\"member member--picture\"></div></li>",
+	
+	"IV-ADCS":"<h1>ADCS</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IVADCS_BhavyaMishra_ECE.jpeg\" id=\"picmem\" title=\"member\"><p>Bhavya Mishra<p><br>ECE</div></li>",
+
+	"III-OBDH":"<h1>OBDH</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIIOBDH_AtikAgrawal_CSE.jpeg\" id=\"picmem\" title=\"member\"><p>Atik Agrawal<p><br>CSE</div></li><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIIOBDH_ThelukuntlaPhaneendra_ECE.jpeg\" id=\"picmem\" title=\"member\"><p>Thelukuntla Phaneendra<p><br>ECE</div></li><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIIOBDH_SuryanshSingh_CSE.jpeg\" id=\"picmem\" title=\"member\"><p>Suryansh Singh<p><br>CSE</div></li>",
+	
+	"III-TTC":"<h1>TTC</h1><li class=\"product\"><div class=\"member member--picture\"></div></li>",
+
+	"III-Payload":"<h1>Payload</h1><li class=\"product\"><div class=\"member member--picture\"></div></li>",
+	
+	"III-STM":"<h1>STM</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIISTM_AbhishekSinghParihaar_Mech.jpeg\" id=\"picmem\" title=\"member\"><p>Abhishek Singh Parihaar<p><br>Mech.</div></li><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIISTM_AhanaBajpai_Mech.jpeg\" id=\"picmem\" title=\"member\"><p>Ahana Bajpai<p><br>Mech.</div></li><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIISTM_PriyanshuKumar_Mech.jpeg\" id=\"picmem\" title=\"member\"><p>Priyanshu Kumar<p><br>Mech.</div></li>",
+	
+	"III-EPS":"<h1>EPS</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIIEPS_TanayPaltanwale_ECE.jpg\" id=\"picmem\" title=\"member\"><p>Tanay Paltanwale<p><br>ECE</div></li>",
+	
+	"III-ADCS":"<h1>ADCS</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIIADCS_ShishankSharma_ECE.jpeg\" id=\"picmem\" title=\"member\"><p>Shishank Sharma<p><br>ECE</div></li>",
+
+	"II-OBDH":"<h1>OBDH</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIOBDH_AbhishekTiwari_CSE.jpg\" id=\"picmem\" title=\"member\"><p>Abhishek Tiwari<p><br>CSE</div></li>",
+	
+	"II-TTC":"<h1>TTC</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IITTC_ArchitChibbar_ECE.jpeg\" id=\"picmem\" title=\"member\"><p>Archit Chibbar<p><br>ECE</div></li><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IITTC_AdityaAgrawal_ECE.jpeg\" id=\"picmem\" title=\"member\"><p>Aditya Agrawal<p><br>ECE</div></li>",
+
+	"II-Payload":"<h1>Payload</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIPayload_PranshKhubchandani_MSME.jpeg\" id=\"picmem\" title=\"member\"><p>Pransh Khubchandani<p><br>MSME</div></li>",
+	
+	"II-STM":"<h1>STM</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IISTM_SauravGulia_Mech.jpeg\" id=\"picmem\" title=\"member\"><p>Saurav Gulia<p><br>Mech.</div></li>",
+	
+	"II-EPS":"<h1>EPS</h1><li class=\"product\"><div class=\"member member--picture\"></div></li>",
+	
+	"II-ADCS":"<h1>ADCS</h1><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIADCS_AnushkaMishra_ECE.jpeg\" id=\"picmem\" title=\"member\"><p>Anushka Mishra<p><br>ECE</div></li><li class=\"product\"><div class=\"member member--picture\"><img src=\"members/IIADCS_MananDubey_ECE.jpeg\" id=\"picmem\" title=\"member\"><p>Manan Dubey<p><br>ECE</div></li>",
+}
